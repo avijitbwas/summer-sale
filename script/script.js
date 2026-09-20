181c396803f6821527e8d80a2c3129789be0f45b
Avijit Biswas <per.useravi0@gmail.com>
2026-09-20 12:58:16 +0600

    added card data script

---
+++ script/script.js
@@ -0,0 +1,30 @@
+document.getElementById('Sportswear-card-1').addEventListener('click',function () {
+    const productName = cardId('Sportswear-card-1');
+
+})
+
+document.getElementById('Sportswear-card-2').addEventListener('click',function () {
+    const productName = cardId('Sportswear-card-2');
+
+})
+
+
+document.getElementById('Sportswear-card-3').addEventListener('click',function () {
+    const productName = cardId('Sportswear-card-3');
+
+})
+
+//kitchenware-card
+
+document.getElementById('kitchenware-card-1').addEventListener('click',function () {
+    const productName = cardId('kitchenware-card-1');
+
+})
+document.getElementById('kitchenware-card-2').addEventListener('click',function () {
+    const productName = cardId('kitchenware-card-2');
+
+})
+document.getElementById('kitchenware-card-3').addEventListener('click',function () {
+    const productName = cardId('kitchenware-card-3');
+
+})
